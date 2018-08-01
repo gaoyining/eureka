61a57a6909aff277e5305e25928e7ceafa8fbc88
yining.gao <yining.gao@msxf.com>
2018-08-01 15:26:51 +0800

    第一阶段最后一次提交，eureka暂时告一段落

--- eureka-client/src/main/java/com/netflix/discovery/EurekaEventListener.java
+++ eureka-client/src/main/java/com/netflix/discovery/EurekaEventListener.java
@@ -3,6 +3,9 @@ package com.netflix.discovery;
 /**
  * Listener for receiving {@link EurekaClient} events such as {@link StatusChangeEvent}.  Register
  * a listener by calling {@link EurekaClient#registerEventListener(EurekaEventListener)}
+ *
+ * 监听器接收{@link EurekaClient}事件，例如{@link StatusChangeEvent}。
+ * 通过调用{@link EurekaClient #registerEventListener（EurekaEventListener）}注册一个监听器
  */
 public interface EurekaEventListener {
     /**
